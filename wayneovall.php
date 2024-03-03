f4dde952fac86807b467fcf59cae6685a9b46b0f
王希銘 <125672114+Cminwang@users.noreply.github.com>
2024-03-03 19:36:52 +0800

    Add files via upload

---
+++ wayneovall.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+</head>
+<body>
+     
+ <?php
+
+   echo "Hello PHP!!!";
+   echo "<br/>";
+   echo "wayneovall is best rapper";
+
+   $a = 1;
+   $b = 2;
+
+   if($a != $b){
+    echo "ovaworld";
+
+   } 
+ ?>
+</body>
+</html>
